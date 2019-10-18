ef58f01f3b08acd3a1cd83a6a1ad1e06dfb5aff7
ITStan <s.stanislavskaya@gmail.com>
2019-10-18 08:37:21 +0300

    Solution

---
+++ src/script.js
@@ -0,0 +1,41 @@
+'use strict'
+
+let data = {
+  "Рыбы": {
+    "форель": {},
+    "лосось": {}
+  },
+
+  "Деревья": {
+    "Огромные": {
+      "секвойя": {},
+      "дуб": {}
+    },
+    "Цветковые": {
+      "яблоня": {},
+      "магнолия": {}
+    }
+  }
+};
+
+createTree(document.getElementById('container'), data);
+
+function createTree(container, data) {
+  container.append(createTreeFromObject(data));
+}
+
+
+function createTreeFromObject(data) {
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li'); // используем рекурсию
+    li.textContent = key;
+
+    if (typeof data[key] === 'object') {
+      li.append(createTreeFromObject(data[key]));
+    }
+    ul.append(li);
+  }
+  return ul;
+}
